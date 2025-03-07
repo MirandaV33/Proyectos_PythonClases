52cb2dd39d47b58b8689cffb368b2f7e9448ef84
MirandaV33 <mimiviceconte@gmail.com>
2025-03-07 09:47:36 -0300

    Primer commit

---
+++ Notas python clase.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  6 18:24:59 2025
+
+@author: l
+"""
+## Si me sale un error con el uso de python, puedo crear UN ENTORNO es mas profesional. 
+
+##Diferencias entre python y c: no tiene LLAVES, hay que tabularlo (ordenarlo) con sangrias.
+##No se usan punto y coma, tiene nombre de funciones parecidas a matlab 
+
+##GITHUB: es una nube donde se guardan TODAS las versiones y con el link puede acceder cualquiera. 
+##Jupyter notebook: es como un word muchisimo mas comodo.
+
+##3 COMANDOS IMPORTANTES
+## PULL: novedades del servidor 
+## PUSH: subir a la nube
+## STATUS: 
+## COMMIT: 
+
+##GitKratten: interfaz gratuita rapida.
+
+##--->ERROR 
+##error: Your local changes to the following files would be overwritten by merge:
+##En mi archivo local lo CAMBIE, entonces me dice que tengo en la nube una NUEVA version 
+##Entonces, deberia elegir que combinar manualmente (MUY DIFICIL)
+##-----> Lo que deberia hacer es SIEMPRE hacer un pull y bajarme las cosas de la nube antes de cambiar cualquier cosa 
+
+##Proyector de notebook:  nbviewer 
+    
+##Abrir jupyter notebook
+##Abre un servidor de aplicaciones web en tu computadora. 
+##--->En el cmd ponemos jupyter-notebook &  
+##APRENDER LATEX ???????
+
+## que carajos es un mersh ?????????????
+ 
+##ABRIR UNA VENTANA CON LOS GRAFICOS: %matplotlib auto (enter) %matplotlib qt
+##Si queremos volver a los graficos adentro: %matplotlib inline 
+
+##GRAFICOS: Las fucniones por defecto, cuando le mandamos vectores (grillas) puntos, lo que hace es INTERPOLARLOS. 
+##Por eso, es que la f0 determina la cantidad de puntos por ciclo y la RESOLUCION del grafico
+##Cerrar todas las ventanas: plt.close('all')
+
+
+
+
+

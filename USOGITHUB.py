f0de34f3f251a981778ba4f90f9b5cfdb3ea15d4
MirandaV33 <mimiviceconte@gmail.com>
2025-03-07 10:21:14 -0300

    Agregar archivo al repositorio

---
+++ USOGITHUB.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  7 09:57:08 2025
+
+@author: l
+"""
+
+##¿Como inicializar y conectar con un repositorio remoto?
+
+echo "# TrabajosPracticos" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MirandaV33/TrabajosPracticos.git
+git push -u origin main
+
+##IMPORTANTE
+##Si creo nuevos repositorios, para no tener conflicto TENGO que cerrar el anterior, 
+##porque los repositorios remoto se conectan al local 
+
+git remote remove origin
+
+## ¿Como agrego archivos desde mi repositorio local? 
+

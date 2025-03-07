38b0033528c99f4d3575f19a07bb364d0e15e0a7
MirandaV33 <mimiviceconte@gmail.com>
2025-03-07 10:43:21 -0300

    Actualizacion de cambios

--- USOGITHUB.py
+++ USOGITHUB.py
@@ -4,15 +4,26 @@ Created on Fri Mar  7 09:57:08 2025
 
 @author: l
 """
+##Conectar cuenta de github
+git config --global user.name "(usuario)"
+git config --global user.email "(mail)"
+##Vincular carpeta
+cd ##Nombredelacarpeta
+git init
+git remote add origin ##Linkdelrepositorio !!Recomiendo cerarlo desde la pagina de github mas facil que crearlo desde los comandos
+git remote -v
+git add . 
+git commit "Primer commit"
+git push -u origin main 
+##Esto sube TODO lo que tengas es tu carpeta 
 
 ##¿Como inicializar y conectar con un repositorio remoto?
-
-echo "# TrabajosPracticos" >> README.md
+echo "# Nombre del repositorio remoto" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/MirandaV33/TrabajosPracticos.git
+git remote add origin ##Linkdelrepositorio
 git push -u origin main
 
 ##IMPORTANTE

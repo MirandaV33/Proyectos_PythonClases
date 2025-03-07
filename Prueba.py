184689f564b75c5471ce3a7940420b84638ab55b
MirandaV33 <mimiviceconte@gmail.com>
2025-03-07 09:40:11 -0300

    Primer commit: agregando archivos al proyecto

---
+++ Prueba.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  6 19:50:11 2025
+
+@author: l
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+fs=1000 ##Frecuencia e muestreo
+f0=2001 ##Frecuencia 
+N=1000 ##Cantidad de muestras 
+
+ts= 1/fs ##Tiempo de muestreo
+ds= fs/N ##Resolucion espectral 
+
+##Grilla de sampleo temporal
+tt= np.linspace(0, (N-1)*ts, N)
+
+##Grilla de sampleo en x 
+xx= np.sin(2*np.pi*f0*tt)
+
+plt.figure(1)
+line_hdls = plt.plot(tt, xx)
+plt.title('Señal: ')
+plt.xlabel('tiempo [segundos]')
+plt.ylabel('Amplitud')
+
+##Cuando f0 es 1, vamos a tener por defecto MUCHAS muestras por segundo --->1000
+## fs/f0=muestrasporciclo
+##Cuando f0 es 400, esta muy cerca de fs/2 por lo que el comportamiento es ERRATICO. 
+
+##TAREA
+##¿Por que no da una linea plana?
+##¿Que pasa cuando desplazamos la senoidal en pi/2?
+## Que pasa cuando vamos cambiando f0. 
+
